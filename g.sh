3e9248deeccc785bd43ff25b2ec3f4f9ca7473c6
Christopher Hoover <ch@murgatroid.com>
2024-02-13 16:09:23 -0800

    Now works with latest tsnet.

--- g.sh
+++ g.sh
@@ -4,8 +4,10 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-export GOLANG="${HOME}/.go/go1.21.3"
+export GOLANG="${HOME}/.go/go1.22.0"
 
 PATH="${GOLANG}/bin:$PATH"
 
+go clean
+go get tailscale.com/tsnet
 go build ./cmd/tsproxy
